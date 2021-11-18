e04354cdf1b3c986e123d5d714852915cb821bb1
Dylan <dylan.wansbrough@hotmail.com>
2021-11-18 15:25:53 +1300

    portal changes

--- Assets/Scripts/POIController.cs
+++ Assets/Scripts/POIController.cs
@@ -1,12 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class POIController : MonoBehaviour
 {
 
     public static int PortalHealth = 20;
     public static bool gameOver = false;
+    public SpriteRenderer inner;
+
+    float alpha = 0.2f;
+    bool up;
+    Color col = new Color(0, 0.9418654f, 1);
 
     private void Update()
     {
@@ -15,6 +21,20 @@ public class POIController : MonoBehaviour
             gameOver = true;
             Debug.Log("Game be Over");
         }
+
+        if(up){
+          alpha += 0.001f;
+          if(alpha > 0.4f){
+            up =  false;
+          }
+        }else{
+          alpha -= 0.001f;
+          if(alpha < 0f){
+            up =  true;
+          }
+        }
+        col.a = alpha;
+        inner.color = col;
     }
 
 
